55089916a6991986cd6c41cb7dfef35f187ee668
R4yya <121268707+R4yya@users.noreply.github.com>
2023-12-24 02:09:02 +0300

    Update whiteboard_interface.py

--- simple_whiteboard/whiteboard_interface.py
+++ simple_whiteboard/whiteboard_interface.py
@@ -11,10 +11,19 @@ class WhiteboardInterface:
 
         menubar = tk.Menu(root)
         root.config(menu=menubar)
+
         file_menu = tk.Menu(menubar, tearoff=0)
         menubar.add_cascade(label='File', menu=file_menu)
         file_menu.add_command(
             label='Save As',
             command=self.whiteboard_controls.save_board,
             accelerator='Ctrl+S'
+        )
+
+        edit_menu = tk.Menu(menubar, tearoff=0)
+        menubar.add_cascade(label='Edit', menu=edit_menu)
+        edit_menu.add_command(
+            label='Undo',
+            command=self.whiteboard_controls.undo_last_action,
+            accelerator='Ctrl+Z'
         )
