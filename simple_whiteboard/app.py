1781f309f559a2a2ee528ea4a28f0edab09fc754
R4yya <121268707+R4yya@users.noreply.github.com>
2023-12-23 22:46:22 +0300

    Update app.py

--- simple_whiteboard/app.py
+++ simple_whiteboard/app.py
@@ -1,10 +1,13 @@
 import tkinter as tk
+from interface import WhiteboardInterface
 
 
-class WhiteboardApp:
+class WhiteboardApp(object):
     def __init__(self, root):
         self.root = root
         self.root.title('Simple Whiteboard')
+
+        self.whiteboard_interface = WhiteboardInterface(root)
 
 
 if __name__ == '__main__':
